cc4145c058d7a3c6b80ac16b67d29efbdc7ca59d
Mateusz Scirka <mscirka@gmail.com>
2015-09-13 16:29:18 +0200

    added skill button

--- js/scenes/skills.js
+++ js/scenes/skills.js
@@ -7,19 +7,37 @@ Q.scene( "Skills", function( stage ){
 		y: Q.height / 2, 
 		fill: "rgba(0,255,0,0.5)"
 	}));
-
-	var buttonClose = container.insert( new Q.UI.Button({
+	
+	var buttonBuyLife = container.insert( new Q.UI.Button({
 		x: 30,
 		y: 30,
+		fill: "#CCCCCC",
+		label: "Buy a life (200 points)"
+	}));
+	
+	var buttonClose = container.insert( new Q.UI.Button({
+		x: 30,
+		y: 100,
 		fill: "#CCCCCC",
 		label: "Close"
 	}));
 
 	
 	buttonClose.on( "click", function() {
-		this.destroy();
+		container.destroy();
 		Q.stage().unpause();
 	});	
+	
+	buttonBuyLife.on( "click", function() {
+		var points = Q.state.get( "score" );
+		
+		if( points >= 200 ) {
+			Q.state.dec( "score", 200 );
+			Q.state.inc( "healths", 1 );
+		} else {
+			alert( "Not enough points!" );
+		}
+	});
 	
 	container.fit( 20 );
 });
